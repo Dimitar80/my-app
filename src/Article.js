af81b792dca9fc9e70a541527fc7028d423a11db
Dimitar80 <designcrafts.mk@gmail.com>
2019-09-14 16:58:44 +0200

    React-My-App-14092019

---
+++ src/Article.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+// CLASS //
+const Article = () => {
+    return (
+       <React.Fragment>
+        {/* <div className='art'> */}
+        <div id='greeting' className='art'>Hello Dimitar!</div>
+        <div className='art'>This is my first app</div>
+        {/* </div> */}
+       </React.Fragment> 
+    )
+}
+
+export default Article
--- src/index.js
+++ src/index.js
@@ -1,24 +1,58 @@
 // alert('Hello World!')
-
 // console.log("Test")
 
 import ReactDOM from 'react-dom'
 import React from 'react'
 import User from './User'
 import Heading from './Heading'
+import Article from './Article'
 
 
 const app = document.getElementById('app')
 
+// const name = 'Dimitar Pavlov'
+// const element = <h1>Hi, I am {name}</h1>;
+
+
+// CLASS //
 const Container = () => {
     return (
         <React.Fragment>
-        <Heading />
-        <User />
+         <Heading />
+         <User />
+         <Article />
         </React.Fragment>
     )
 }
-ReactDOM.render(<Container/>, app)
 
+ReactDOM.render(<Container/>, app);
+
+
+// PROBA START //
+// (function sayHello() {
+//     alert("hello DP!");
+// })();
+
+//  function sayHello() {
+//     alert("hello DP!");
+//  };
+//   sayHello();
+
+// class Rectangle {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }
+//   }
+//   alert(Rectangle);
+
+// let Rectangle = class Rectangle2 {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }
+//   };
+//   alert(Rectangle.name);
+  // PROBA END //
 
 
